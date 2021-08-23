b7c8e4b6cfa0255f658c1f62b2ac71578bb0feda
aisha428 <89053335+aisha428@users.noreply.github.com>
2021-08-23 16:22:42 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,7 +6,15 @@ var g = 0;
 var b = 0;
 
 function setup() {
-  createCanvas(400, 400);  
+  createCanvas(400, 400);
+  
+  btn_red = createButton("RED");
+  btn_red.position(100,50);
+  btn_red.mousePressed(red_bg);
+  
+  btn_green = createButton("GREEN");
+  btn_green.position(250,50);
+  btn_green.mousePressed(green_bg);
 }
 
 function draw() {
